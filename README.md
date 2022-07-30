39573da951587648f2f4c1cb61455b6f1c8136b3
vladimir <vladimirfomin90@yandex.ru>
2022-07-31 00:35:39 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project5-jsLibrary
+# project5-jsLibrary
---
+++ src/js/lib/core.js
@@ -0,0 +1,46 @@
+// (() => {
+//     const $ = function (selector) {
+//         const elements = document.querySelectorAll(selector);
+//         const obj = {};
+
+//         obj.hide = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = 'none';
+//             });
+//             return obj;
+//         };
+
+//         obj.show = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = ''; //браузер сам решит flex или block
+//             });
+//             return obj;
+//         };
+
+//         return obj;
+        
+//     };
+
+//     window.$ = $;
+
+// })();
+
+const $ = function (selector) {
+    return new $.prototype.init(selector);
+};
+
+$.prototype.init = function(selector) {
+    if(!selector) {
+        return this; // {} пустой объект
+    }
+
+    Object.assign(this, document.querySelectorAll(selector));
+    this.length = document.querySelectorAll(selector).length;
+    return this;
+};
+
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;
